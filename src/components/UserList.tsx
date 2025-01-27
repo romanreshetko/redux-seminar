7823e56eb74ed377312e9a4992053c446e8d486b
Roman Reshetko <rvreshetko@edu.hse.ru>
2025-01-27 14:26:55 +0300

    added navigation

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { List, ListItem, ListItemText, Typography, Container } from "@mui/material";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+import { useNavigate } from "react-router";
 
 
 const UserList: React.FC = () => {
     const usersList = useSelector((state: RootState) => state.users);
+    const navigate = useNavigate();
 
     return (
         <Container maxWidth="sm" style={{ marginTop: "2rem" }}>
@@ -14,7 +16,7 @@ const UserList: React.FC = () => {
             </Typography>
             <List>
                 {usersList.map((user, index) => (
-                    <ListItem key={index} divider>
+                    <ListItem key={index} divider onClick={() => navigate(`/user/${index}`)}>
                         <ListItemText
                             primary={user.name}
                             secondary={`Age: ${user.age}`}
